7a9bc67638d8a4268695c13ac171f11fb38e4486
John Mettraux <jmettraux@gmail.com>
2016-06-14 16:26:01 +0900

    add :first => xxx

--- test.rb
+++ test.rb
@@ -8,7 +8,6 @@ p [ :ruby, RUBY_VERSION ]
 p [ :rufus_scheduler, Rufus::Scheduler::VERSION ]
 p [ :uname_a, `uname -a`.strip ]
 
-Timecop.travel(Time.local(2016, 6, 11, 23, 59, 55))
 
 puts
 p Time.now
@@ -16,13 +15,25 @@ puts "scheduling...\n"
 
 s = Rufus::Scheduler.new
 
+Timecop.travel(Time.local(2016, 1, 1, 0, 0, 0))
+puts "...travelled to #{Time.now}"
+
+s.cron '0 0 * * sun', first: "3d" do
+  t = Time.now
+  p [ t, t.wday, :hello, 'first: "3d"' ]
+end
+
+#Timecop.travel(Time.local(2016, 6, 11, 23, 59, 55))
+Timecop.travel(Time.local(2016, 6, 11, 23, 59, 50))
+puts "...travelled to #{Time.now}"
+
 s.cron '0 0 * * 0' do
   t = Time.now
-  p [ t, t.wday, :hello, 0 ]
+  p [ t, t.wday, :hello, '0' ]
 end
 s.cron '0 0 * * sun' do
   t = Time.now
-  p [ t, t.wday, :hello, :sun ]
+  p [ t, t.wday, :hello, 'sun' ]
 end
 
 s.join
